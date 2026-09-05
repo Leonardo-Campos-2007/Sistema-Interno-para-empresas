cfde36bdf50e26f075dc6d34906e81a7016293f1
Leonardo-Campos-2007 <leonardogomescampos2007@gmail.com>
2026-09-05 11:47:53 -0300

    Inicio do projeto

---
+++ src/main/java/com/br/sistemainternoparaempresas/SistemaInternoParaEmpresasApplication.java
@@ -0,0 +1,13 @@
+package com.br.sistemainternoparaempresas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SistemaInternoParaEmpresasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SistemaInternoParaEmpresasApplication.class, args);
+    }
+
+}
